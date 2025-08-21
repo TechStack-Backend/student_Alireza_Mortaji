a2064b881c30a8ec946cb07051c857a01ba8caf3
alireza841384 <alireza.m.841384@gmail.com>
2025-08-21 18:27:52 +0430

    <feat>(task_2): implement urls

--- task2/listDevelopers/urls.py
+++ task2/listDevelopers/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Developers.views import listDevelporsView,CV_View
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Developers/',listDevelporsView),
+    path('Developers/<str:username>/',CV_View),
 ]
