608a739ebb1fb6c60d320e35f4c672f1f512f530
alireza841384 <alireza.m.841384@gmail.com>
2025-09-04 12:09:24 +0430

    feat(task_2):Initial implementation of views

--- task_2_project/developersView/views.py
+++ task_2_project/developersView/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+Develpers = [
+    {
+        "username": "hassan",
+        "first_name": "Hassan",
+        "last_name": "Kabirian",
+        "skills": ["Python", "Django", "Vue.js"],
+    },
+    {
+        "username": "sara",
+        "first_name": "Sara",
+        "last_name": "Ahmadi",
+        "skills": ["JavaScript", "React", "CSS"],
+    },
+    {
+        "username": "ali",
+        "first_name": "Ali",
+        "last_name": "Rezayi",
+        "skills": ["Java", "Spring Boot", "SQL"],
+    },
+]
+
+
+def DevelopersListView(request):
+    if request.method == "GET":
+        context = {'developers': Develpers}
+        pass
+
+
+def DeveloperCV_View(request, username):
+    if request.method == "GET":
+        for dev in Develpers:
+            if dev['username'] == username:
+                context = {"developer": dev}
+                pass
