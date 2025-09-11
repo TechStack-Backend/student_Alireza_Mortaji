610caf87ca436680ccc51275d07791390699b530
alireza841384 <alireza.m.841384@gmail.com>
2025-09-11 12:40:42 +0430

    feat(task_3):implement models for developers,skills and project

--- task3/developers/models.py
+++ task3/developers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Developer(models.Model):
+    first_name = models.CharField(max_length=30, verbose_name='name')
+    last_name = models.CharField(max_length=60, verbose_name='family')
+    email = models.EmailField()
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'developers'
+        ordering = ["first_name", "last_name"]
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.TextField()
+    developer = models.ForeignKey(
+        Developer, related_name='skills', on_delete=models.CASCADE)
