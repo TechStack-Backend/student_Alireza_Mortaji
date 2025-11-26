62db49950e8eebb4229f5180ade71aadb7e14e02
alireza841384 <alireza.m.841384@gmail.com>
2025-11-26 17:37:24 +0330

    feat(task_5):add auth urls and implement register view

---
+++ task3/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth
+from .views import registerView
+
+urlpatterns = [
+    path("login/", auth.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path("logout/", auth.LogoutView.as_view(next_page="login"), name="logout"),
+    path("register/", registerView.as_view(), name="register")
+]
